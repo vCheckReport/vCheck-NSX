695d0985fa913a9c6548c7709aedaf7984156773
David Hocking <dave@thehockings.net>
2017-04-07 09:48:13 +0100

    Added cluster exclusion regex option, reworked the output to show which cluster has an issue (for vCenters with more than one cluster)

--- Plugins/15 NSX/175 NSX Cluster Preparation State.ps1
+++ Plugins/15 NSX/175 NSX Cluster Preparation State.ps1
@@ -1,43 +1,48 @@
 # Start of Settings
+# Specify regex to filter OUT any clusters connected to the vCenter - otherwise all clusters assumed to be NSX enabled
+$exclude = ""
 # End of Settings 
 
-# Get clusters from vcenter - bit dirty as it will collect all clusters, not just nsx prepared ones
-$clusters = get-cluster 
-
-# get nsx cluster status from clusters
-$nsxclusters = $clusters | Get-NsxClusterStatus
+# Get clusters from vcenter
+$clusters = get-cluster | Where-Object {$_.name -notmatch $exclude}
 
 # Build the table that will hold the health data
 $NsxClusterHealthTable = New-Object system.Data.DataTable "NSX Cluster Preparation State"
 
 # Define Columns
 $cols = @()
+$cols += New-Object system.Data.DataColumn "Cluster Name",([string])
 $cols += New-Object system.Data.DataColumn "Feature",([string])
 $cols += New-Object system.Data.DataColumn "Host Preparation State",([string])
 $cols += New-Object system.Data.DataColumn "Installed",([string])
 $cols += New-Object system.Data.DataColumn "Enabled",([string])
 
-    
 #Add the Columns
 foreach ($col in $cols) {$NsxClusterHealthTable.columns.add($col)}
 
-foreach ($nsxFeature in $nsxclusters)
+foreach ($cluster in $clusters)
 {
-    # Populate a row in the Table
-    $row = $NsxClusterHealthTable.NewRow()
+    # get nsx cluster status from cluster
+    $nsxcluster = $cluster | Get-NsxClusterStatus
 
-    # Enter data in the row
-    $row.Feature  = ($nsxFeature.featureId).split('.')[-1]
-    $row."Host Preparation State" = if($nsxFeature.status -match 'RED|UNKNOWN' ){"Not Ready"}
-    $row.Installed = $nsxFeature.installed
-    $row.Enabled = $nsxFeature.enabled
+    foreach ($nsxFeature in $nsxcluster)
+    {
+        # Populate a row in the Table
+        $row = $NsxClusterHealthTable.NewRow()
+
+        # Enter data in the row
+        $row."Cluster Name" = $cluster.name
+        $row.Feature  = ($nsxFeature.featureId).split('.')[-1]
+        $row."Host Preparation State" = if($nsxFeature.status -match 'RED|UNKNOWN' ){"Not Ready"}
+        $row.Installed = $nsxFeature.installed
+        $row.Enabled = $nsxFeature.enabled
                 
-    # Add the row to the table
-    $NsxClusterHealthTable.Rows.Add($row)
+        # Add the row to the table
+        $NsxClusterHealthTable.Rows.Add($row)
+    }
 }
-
 # Display the Status Table if there's an issues
-$NsxClusterHealthTable | Select-Object Feature,"Host Preparation State","Installed","Enabled" | where {$_."Host Preparation State" -eq "Not Ready" -or $_."Installed" -eq "false" -or $_."Enabled" -eq "false"}
+$NsxClusterHealthTable | Select-Object "Cluster Name",Feature,"Host Preparation State","Installed","Enabled" | where {$_."Host Preparation State" -eq "Not Ready" -or $_."Installed" -eq "false" -or $_."Enabled" -eq "false"}
 
 # Plugin Outputs
 $PluginCategory = "NSX"
@@ -46,6 +51,4 @@ $Header = "NSX Cluster Preparation State"
 $Comments = "NSX cluster(s) are reporting preparation health issues"
 $Display = "Table"
 $Author = "Dave Hocking / Adam Baron"
-$PluginVersion = 0.1
-
-
+$PluginVersion = 0.2
